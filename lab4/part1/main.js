c116660dc88c37b380ce0414f1f4953c4b2322bc
manishSingh077 <manish.singh@dcmail.ca>
2023-11-24 15:21:55 -0500

    final commit
    All done

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -5,7 +5,7 @@ const story = document.querySelector('.story');
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
-
+}
 
 const storyText="It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
@@ -18,18 +18,23 @@ const insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk
 
 randomize.addEventListener('click', result);
 
-function result() {
+function result() 
+{
   let newStory= storyText;
 
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory= newStory.replaceAll("Bob",name);
 
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weightInStones=Math.round(300 / 14);
+    const temperatureInCentigrade = (Math.round(94-32)*5/9);
+    newStory= newStory.replaceAll("94 fahrenheit",temperatureInCentigrade);
+    newStory=newStory.replaceAll("300 pounds", weightInStones);
+
 
   }
 
@@ -37,10 +42,10 @@ function result() {
   const yItem= randomValueFromArray(insertY);
   const zItem= randomValueFromArray(insertZ);
 
-  newStory= newStory.replaceAll(":inseertx:", xItem);
-  newStory= newstory.replaceAll(":inserty:", yItem);
+  newStory= newStory.replaceAll(":insertx:", xItem);
+  newStory= newStory.replaceAll(":inserty:", yItem);
   newStory= newStory.replaceAll(":insertz:", zItem);
 
   story.textContent = newStory ;
-  story.style.visibility = 'visible';
+  story.style.visibility = 'visible'; 
 }
